7662638d702a6544243ec5416fb8dba91a31f6aa
Ivan Starčević <ivan.starcevic15@gmail.com>
2021-12-20 20:33:51 +0100

    Refactoring routes

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
-const uuid = require("uuid");
-const fs = require("fs");
 const path = require("path");
 
-const resData = require("./util/restaurant-data");
+const defaultRoutes = require("./routes/default");
+const restaurantRoutes = require("./routes/restaurants");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -12,56 +11,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.get("/restaurants", (req, res) => {
-  const storedRestaurants = resData.getStoredRestaurants();
-
-  res.render("restaurants", {
-    numberOfRestaurants: storedRestaurants.length,
-    restaurants: storedRestaurants,
-  });
-});
-
-app.get("/restaurants/:id", (req, res) => {
-  const restaurantId = req.params.id;
-
-  const storedRestaurants = resData.getStoredRestaurants();
-
-  for (const restaurant of storedRestaurants) {
-    if (restaurant.id === restaurantId) {
-      return res.render("restaurant-detail", { restaurant: restaurant });
-    }
-  }
-
-  res.status(404).render("404");
-});
-
-app.get("/recommend", (req, res) => {
-  res.render("recommend");
-});
-
-app.post("/recommend", (req, res) => {
-  const restaurant = req.body;
-  restaurant.id = uuid.v4();
-  const storedRestaurants = getStoredRestaurants();
-
-  storedRestaurants.push(restaurant);
-
-  resData.storeRestaurants(storedRestaurants);
-
-  res.redirect("/confirm");
-});
-
-app.get("/confirm", (req, res) => {
-  res.render("confirm");
-});
-
-app.get("/about", (req, res) => {
-  res.render("about");
-});
+app.use("/", defaultRoutes);
+app.use("/", restaurantRoutes);
 
 app.use((req, res) => {
   res.status(404).render("404");
